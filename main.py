0e16194a896e1b2ec69495064e88604ccb3b8ea1
Pascal U. Förster <pascal.foerster.19@ucl.ac.uk>
2021-09-24 20:35:25 +0100

    Add a better flow chart for reference_evolve
    Original plan to have a flow chart to show dependencies on class instance attributes not done yet, since the charts get pretty confusing pretty fast, in that case.
    
    No changes in classes/their methods, just some different initial conditions set in main.py to run for 10 Gyr and have comparable masses mstar and mhalo to the ones used in Fig. 1 of Lilly+13.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def testrun_1(timestep: float = 1.e-3, silent: bool = False):
     EnvA = sgm.Environment(name='Environment A')
     if not silent:
         print(f"Created '{EnvA.name}'")
-    GalA1 = EnvA.create_Galaxy(name='Galaxy A1')
+    GalA1 = EnvA.create_Galaxy(name='Galaxy A1', mgas=1e10, mhalo=10**11.5)
     if not silent:
         print(f"Created '{GalA1.name}'")
         print(EnvA)
@@ -21,7 +21,7 @@ def testrun_1(timestep: float = 1.e-3, silent: bool = False):
     GalA1_history.append(GalA1.make_snapshot())
     if not silent:
         print(f"{0}, z={EnvA.z}, lookbacktime={EnvA.lookbacktime}")
-    for t in range(1000):
+    for t in range(10000):
         EnvA.reference_evolve(timestep=timestep)
         GalA1_history.append(GalA1.make_snapshot())
         if not silent:
@@ -41,14 +41,24 @@ def testrun_1(timestep: float = 1.e-3, silent: bool = False):
     mout_l = [GalA1_history[i]['mout'] for i, item in enumerate(GalA1_history)]
     GAR_l = [GalA1_history[i]['GAR'] for i, item in enumerate(GalA1_history)]
     mhalo_l = [GalA1_history[i]['mhalo'] for i, item in enumerate(GalA1_history)]
+    sMIR_l = [GalA1_history[i]['sMIR'] for i, item in enumerate(GalA1_history)]
+    rsSFR_l = [GalA1_history[i]['rsSFR'] for i, item in enumerate(GalA1_history)]
+    fout_l = [GalA1_history[i]['fout'] for i, item in enumerate(GalA1_history)]
+    fgas_l = [GalA1_history[i]['fgas'] for i, item in enumerate(GalA1_history)]
+    fstar_l = [GalA1_history[i]['fstar'] for i, item in enumerate(GalA1_history)]
 
     fig, ax = plt.subplots(figsize=(9,6))
     # ax.plot(timesteps_l, mgas_l, label="mgas")
     # ax.plot(timesteps_l, mstar_l, label="mstar")
     # ax.plot(timesteps_l, mout_l, label="mout")
-    ax.plot(timesteps_l, GAR_l, label="GAR (gas accretion rate)")
-    ax.plot(timesteps_l, mhalo_l, label="mhalo (total halo mass)")
-    ax.set_yscale('log')
+    # ax.plot(timesteps_l, GAR_l, label="GAR (gas accretion rate)")
+    # ax.plot(timesteps_l, mhalo_l, label="mhalo (total halo mass)")
+    # ax.plot(timesteps_l[5:], sMIR_l[5:], label="sMIR (specific mass increase rate)")
+    # ax.plot(timesteps_l[5:], rsSFR_l[5:], label="rsSFR (reduced specific star formation rate)")
+    ax.plot(timesteps_l[5:], fout_l[5:], label="fout (gas ejected as fraction of infalling gas)")
+    ax.plot(timesteps_l[5:], fgas_l[5:], label="fgas (gas going into reservoir as fraction of infalling gas)")
+    ax.plot(timesteps_l[5:], fstar_l[5:], label="fstar (gas converted to long-lived stars as fraction of infalling gas)")
+    # ax.set_yscale('log')
     ax.legend()
     fig.show()
 
