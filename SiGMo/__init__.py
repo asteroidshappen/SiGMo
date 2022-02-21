d77409a6cd3609c56c31f0bb43204bdee871f669
Pascal U. Förster <pascal.foerster.19@ucl.ac.uk>
2022-02-21 20:43:49 +0000

    __init__ file for the SiGMo package

---
+++ SiGMo/__init__.py
@@ -0,0 +1,4 @@
+from .SiGMo_AstroObject import Environment, Halo, Galaxy
+from .SiGMo_Snapshot import Snapshot
+from .SiGMo_ICs import IC, create_AstroObject_Ensemble
+from .SiGMo_Integrator import FTI
