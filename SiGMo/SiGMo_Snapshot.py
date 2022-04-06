36b12a8cdd81f0c9a22f40dff77b59a60100c6a7
Pascal U. Förster <pascal.foerster.19@ucl.ac.uk>
2022-04-06 13:21:14 +0100

    Move Snapshot read-in routine to SiGMo_Snapshot.py
    Moved the multi read-in routine for single or multi Snapshots from a
    specified directory to the SiGMo_Snapshot.py file. Even though the
    routine is not part of the Snapshot class, it's used to read in large
    numbers of files of this type from a directory, so this location is more
    logical than putting it in SiGMo_misc.py

--- SiGMo/SiGMo_Snapshot.py
+++ SiGMo/SiGMo_Snapshot.py
@@ -1,3 +1,8 @@
+# general
+import numpy as np
+import warnings
+import tqdm
+
 # I/O
 import json
 import os
@@ -30,6 +35,120 @@ def split_path_and_name(inpath, sep='_'):
     basename = '_'.join(meta_attr_strings[1:-1])
     *snaptime, extension = meta_attr_strings[-1].split('.')
     return dirpath, filename, prefix, basename, snaptime, extension
+
+def read_all_snapshots_from_dir(
+        snp_dir,
+        n_envs=None, n_halos=None, n_gals=None,
+        single_snapshots=False, n_steps=None,
+        fn_start = "snp",  # string elements to split and recognise file names by
+        fn_middle_gal = "Galaxy",
+        fn_middle_halo = "Halo",
+        fn_middle_env = "Environment",
+        fn_end = "",
+        fn_separator = "_"
+):
+    """
+    Reads in all snapshot files from a specified directory
+
+    :param snp_dir: Input directory
+    :param n_envs: Number of Environment objects at any point in time during the simulation (usually: 1)
+    :param n_halos: Number of Halo objects at any point in time during the simulation
+    :param n_gals: Number of Galaxy objects at any point in time during the simulation
+    :param single_snapshots: Is the input individual snapshots for each object and time step (True),
+        or one per Environment and time step (False; default)
+    :param n_steps: Number of time steps in the simulation (or simulation output if wtd > 1)
+    :param fn_start: String element used to identify the beginning of snapshot file names
+    :param fn_middle_gal: String element used to identify the middle part of Galaxy snapshot file names
+    :param fn_middle_halo: String element used to identify the middle part of Halo snapshot file names
+    :param fn_middle_env: String element used to identify the middle part of Environment snapshot file names
+    :param fn_end: String element used to identify the end part of snapshot files (can be left empty)
+    :param fn_separator: The separator used to split the full file names in order to analyse/compare them
+    :return: env_grid, halo_grid, gal_grid: numpy arrays with the snapshots of all Environments,
+        Halos and Galaxies over all time steps of the simulation. Example with 2000 time steps, one Environment
+        and 379 Halos in it with one Galaxy each:
+        env_grid.shape=(1, 2000), halo_grid.shape=(379, 2000), gal_grid.shape=(379, 2000)
+    """
+    # get all file names
+    print(f"Reading all snapshots from directory '{snp_dir}'")
+    file_iter = snp_dir.iterdir()
+    file_list = list(file_iter)
+    n_files = len(file_list)
+
+    # how many timesteps?
+    if single_snapshots:
+        # n_steps = 800
+        # n_steps = 799
+        if n_steps is None:
+            raise TypeError("'n_steps' has not been set, but is required for 'single_snapshots=True' mode")
+    else:
+        n_steps = n_files
+
+    # How many envs, halos, gals if numbers not provided?
+    # if number of halos and galaxies is not (properly) provided
+    # WARNING::: this HARDCODES 1 Environment total and same number of Galaxies as Halos (one each)!
+    n_envs = n_envs if n_envs else 1
+    if (not n_halos) or (not n_gals):
+        # find an Environment snapshot file
+        _tmp_filepath = None
+        for _tmp_filepath in file_list:
+            if _tmp_filepath.resolve().name.split(fn_separator)[1] == fn_middle_env:
+                _tmp_snp = Snapshot.load_from_disk(_tmp_filepath)
+                break
+        # set number of Halos
+        if not n_halos:
+            # catch the case where the above didn't find an Environment snapshot file
+            try:
+                n_halos = len(_tmp_snp.data["halos"])
+            except AttributeError:
+                print(f"'n_halos' could not be set, because no Snapshot file with "
+                      f"'{fn_middle_env}' in it/in the right place could be found "
+                      f"in directory '{snp_dir}'")
+        # set number of Galaxies
+        if not n_gals:
+            n_gals = n_halos
+
+    # make arrays here already
+    env_grid = np.empty(shape=(n_envs, n_steps), dtype=object)
+    halo_grid = np.empty(shape=(n_halos, n_steps), dtype=object)
+    gal_grid = np.empty(shape=(n_gals, n_steps), dtype=object)
+
+    # loop trough the files in the arbitrary order that they got in the iterator file_iter
+    for file in tqdm(file_list, total=n_files):
+        fn = file.name
+
+        # check that fn is regular snapshot of time evolution, not other file (e.g. "final" snapshot etc)
+        if fn.startswith(fn_start):
+            # split the file name (not the whole path)
+            fn_split = fn.split(fn_separator)
+
+            # assign the different elements from file name to different counters (time and object/param combination)
+            t = int(fn_split[0][len(fn_start):])
+            i = int(fn_split[2]) if len(fn_split) == 4 else 0
+            snp_type = fn_split[1]
+
+            # make full file path with directories
+            full_file_path = file.resolve()
+
+            # differentiate between Galaxy, Halo and Environment files
+            if snp_type == fn_middle_env:
+                env_grid[..., t].flat[i] = Snapshot.load_from_disk(full_file_path)
+            elif snp_type == fn_middle_halo:
+                if single_snapshots:
+                    halo_grid[..., t].flat[i] = Snapshot.load_from_disk(full_file_path)
+                else:
+                    warnings.warn(f"The type '{snp_type}' of snapshot '{file}' is not yet supported for multi_snapshot "
+                                  f"read in!\nSnapshot was not read in.")
+            elif snp_type == fn_middle_gal:
+                if single_snapshots:
+                    gal_grid[..., t].flat[i] = Snapshot.load_from_disk(full_file_path)
+                else:
+                    warnings.warn(f"The type '{snp_type}' of snapshot '{file}' is not yet supported for multi_snapshot "
+                                  f"read in!\nSnapshot was not read in.")
+            else:
+                warnings.warn(
+                    f"The type '{snp_type}' of snapshot '{file}' was not recognised! Snapshot was not read in.")
+
+    return env_grid, halo_grid, gal_grid
 
 
 # Snapshot Class: custom class with a dict that contains all of one AstroObject's attributes
--- SiGMo/__init__.py
+++ SiGMo/__init__.py
@@ -1,5 +1,5 @@
 from .SiGMo_AstroObject import Environment, Halo, Galaxy
-from .SiGMo_Snapshot import Snapshot
+from .SiGMo_Snapshot import Snapshot, read_all_snapshots_from_dir
 from .SiGMo_ICs import IC, create_AstroObject_Ensemble
 from .SiGMo_Integrator import FTI
 from .SiGMo_misc import *
